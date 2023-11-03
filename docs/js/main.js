5be28f99c637fe870d18ed3d80aa5780298b32b6
georapbox <georapbox@gmail.com>
2023-11-03 15:09:12 +0200

    Update demo

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,9 @@
+import '../lib/browser-window.js';
 const isLocalhost = window.location.href.includes('127.0.0.1') || window.location.href.includes('localhost');
 const componentUrl = isLocalhost ? '../../dist/theme-toggle-defined.js' : '../lib/theme-toggle-defined.js';
-const consoleEl = document.getElementById('console');
 
 import(componentUrl).then(() => {
-  document.addEventListener('theme-toggle:change', evt => {
-    console.log('theme-toggle:change ->', evt.detail);
-    consoleEl.innerHTML = `theme-toggle:change -> ${JSON.stringify(evt.detail)}`;
+  document.addEventListener('theme-change', evt => {
+    console.log('theme-change ->', evt.detail);
   });
 });
